1dc23726b5242a3f6ffe6ce0c16d4fd0493f44ed
aleksey.frundin@gmail.com <aleksey.frundin@gmail.com>
2018-11-23 19:31:24 +0200

    drivers

--- app/t.py
+++ app/t.py
@@ -1,10 +1,1 @@
-
-
-{{obj.conditions_one_title}}
-{{obj.conditions_one_description}}
-
-{{obj.conditions_two_title }}
-{{obj.conditions_two_description}}
-
-{{obj.conditions_three_title}}
-{{obj.conditions_three_description}}
+{{Register as driver}}
